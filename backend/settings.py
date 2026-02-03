b2dbf24d3c5dc1591667910d43e98d984c1ecc6a
Sakthivel P <sakthirollins175@gmail.com>
2026-02-03 13:05:06 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://sparkly-palmier-49425f.netlify.app",
+]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
